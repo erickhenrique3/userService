7ae7583fa8ede0d7c8af88173b54b4aee6abdd7e
ErickHenrique <erick.2henriquehtp@gmail.com>
2025-10-26 15:13:28 -0300

    Refactor server startup logic by removing error handling for port conflicts and simplifying the server initialization process.

--- src/server.ts
+++ src/server.ts
@@ -30,22 +30,9 @@ app.use(userRoutes.routes()).use(userRoutes.allowedMethods());
 const port = Number(process.env.PORT) || 3001;
 
 const startServer = () => {
-  const server = app.listen(port, () => {
+   app.listen(port, () => {
     console.log(`✅ User service running on http://localhost:${port}`);
     console.log(`📄 Swagger docs available on http://localhost:${port}/docs`);
-  });
-
-  server.on("error", (err: any) => {
-    if (err.code === "EADDRINUSE") {
-      console.log(`⚠️ Porta ${port} em uso. Tentando porta ${port + 1}...`);
-      server.close(() => {
-        process.env.PORT = String(port + 1);
-        startServer();
-      });
-    } else {
-      console.error(err);
-      process.exit(1);
-    }
   });
 };
 
